8289fbf21e4c9ac8a36124f584d03e7c0b493691
Robert Rees <robert@wazoku.com>
2011-11-29 20:00:28 +0000

    Working story templates

--- src/story_maker/core.clj
+++ src/story_maker/core.clj
@@ -1,3 +1,11 @@
 (ns story-maker.core)
 
+(def nouns ["house" "cat" "kettle" "potato"])
+(def adjectives ["fast" "quiet" "lemony"])
+
 (defn hello [name] (str "Hello " name))
+
+(defn story ([]
+	(str "The " (rand-nth adjectives) " " (rand-nth nouns)))
+	([name]
+	(str name " was in the barn")))
